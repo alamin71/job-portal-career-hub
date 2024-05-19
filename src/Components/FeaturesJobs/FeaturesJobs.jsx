e642eaa9a24b2ebc7c87cf1e78074a1348c474a4
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-19 23:21:51 +0600

    load job data from json file

---
+++ src/Components/FeaturesJobs/FeaturesJobs.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import Job from "../Job/Job";
+
+
+const FeaturesJobs = () => {
+    const [job, setJob] = useState([]);
+    useEffect(() => {
+        fetch('jobs.json')
+            .then(res => res.json())
+            .then(data => setJob(data))
+    }, [])
+
+    return (
+        <div>
+            <div className="text-center">
+                <h2 className="text-4xl font-bold">Featured Jobs:{job.length}</h2>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                <div>
+                    {
+                        job.map(job => <Job key={job.id} job={job}></Job>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturesJobs;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,16 @@
+import Banner from "../Banner/Banner";
+import CategoryList from "../CategoryList/CategoryList";
+import FeaturesJobs from "../FeaturesJobs/FeaturesJobs";
 
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+            <CategoryList></CategoryList>
+            <FeaturesJobs></FeaturesJobs>
             <h1>This is Home</h1>
+            
         </div>
     );
 };

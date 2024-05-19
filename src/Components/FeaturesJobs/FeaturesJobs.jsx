36fbc0543c089115447d425171959a860bb65492
AL-AMIN <alamin50cse.miu@gmail.com>
2024-05-20 00:35:50 +0600

    job feature data show display by card

--- src/Components/FeaturesJobs/FeaturesJobs.jsx
+++ src/Components/FeaturesJobs/FeaturesJobs.jsx
@@ -15,7 +15,7 @@ const FeaturesJobs = () => {
             <div className="text-center">
                 <h2 className="text-4xl font-bold">Featured Jobs:{job.length}</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-                <div>
+                <div className="grid grid-cols-2 gap-6">
                     {
                         job.map(job => <Job key={job.id} job={job}></Job>)
                     }
